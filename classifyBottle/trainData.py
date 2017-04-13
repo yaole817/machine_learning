a666fb0e25d764c9145e852b2e0b7b16ba3d8ea2
yaole817 <yaole817@163.com>
2017-04-13 19:30:19 +0800

    standard dataset of image,which can use knn to classify
    Signed-off-by: yaole817 <yaole817@163.com>

--- classifyBottle/trainData.py
+++ classifyBottle/trainData.py
@@ -5,29 +5,19 @@ import os
 import sys
 import numpy as np
 dir_path = '.\\res'
-list =  [x for x in os.listdir(dir_path) if os.path.splitext(x)[1]=='.jpg']
-path = dir_path+'\\'+list[int(sys.argv[1])]
+fileList =  [x for x in os.listdir(dir_path) if os.path.splitext(x)[1]=='.jpg']
+path = dir_path+'\\'+fileList[int(sys.argv[1])]
 
-if __name__ == '__main__':
+def img2Vect(img,lable):
+    image = imgLib.colorDetect(img,0) # detect the blue colour
+    imageBlue   = imgLib.findBottleCap(image)  # find the bottle area in one picture
 
-    img = cv2.imread(path)
-
-    imgR=img[:,:,0]
-    imgG=img[:,:,1]
-    imgB=img[:,:,2]
-    image = imgG-imgB
-    
-    #image = imageGray(path)
-    image = imgG
-    
-    image = imgLib.colorDetect(img,0)
-    imageBlue   = imgLib.findBottleCap(image)
-
-    image = imgLib.colorDetect(img,1)
+    image = imgLib.colorDetect(img,1) # detect yellow colour
     imageYellow = imgLib.findBottleCap(image)
-    #image = imgLib.findTextRegion(image)
+
     imageYellow.extend(imageBlue)
-    trainData=[]
+    trainData = []
+    lableData = []
     for box in imageYellow:
         cutImg= imgLib.cutImg(box,img)
         gray = cv2.cvtColor(cutImg,cv2.COLOR_BGR2GRAY) 
@@ -35,11 +25,29 @@ if __name__ == '__main__':
         imgLib.cutImageFromCircle(gray)
         hist = cv2.calcHist([gray], [0], None, [256], [1.0,255.0])
         trainData.append(hist)
+        lableData.append(lable)
+    return trainData,lable
+
+def loadImages(filepath):
+
+    lable = filepath.split('\\')[-1].replace('.jpg','')[:-2]
+
+    img = cv2.imread(filepath)
+    trainData,lable = img2Vect(img,lable)
+    print lable
+    return trainData,lable 
+
+if __name__ == '__main__':
+    trainDatas = []
+    lables = []
+    for item in fileList:
+        trainData,lable = loadImages(dir_path+'\\'+item)
+        trainDatas.extend(trainData)
+        lables.extend(lable)
+    with open('dataSet.txt','w') as f:
+        f.write(''.join(str(trainDatas)))
+        f.write('\n'.join(lables))
         #cv2.imshow('img',gray)
         
         #imgLib.grayHist(gray)
         #cv2.waitKey(0)
-    with open('trainData.txt','w') as f:
-    	for i in trainData:
-    		f.write(str(i))
-    		f.write('\n')
