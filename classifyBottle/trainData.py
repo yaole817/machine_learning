b0b671d1eb372493053cf2983fa544f9575f154f
yaole817 <yaole817@163.com>
2017-04-16 19:13:36 +0800

    implement image knn
    Signed-off-by: yaole817 <yaole817@163.com>

--- classifyBottle/trainData.py
+++ classifyBottle/trainData.py
@@ -4,7 +4,7 @@ import cv2
 import os
 import sys
 import numpy as np
-dir_path = '.\\res'
+dir_path = '.\\database'
 fileList =  [x for x in os.listdir(dir_path) if os.path.splitext(x)[1]=='.jpg']
 path = dir_path+'\\'+fileList[int(sys.argv[1])]
 
@@ -40,7 +40,47 @@ def loadImages(filepath):
     trainData,lable = img2Vect(img,lable)
     return trainData,lable 
 
+def gray2GrayHist(gray):
+
+    hist = cv2.calcHist([gray], [0], None, [256], [1.0,255.0])
+    hist = np.int0(hist)
+    grayHistArray = hist[:,0]
+
+    return grayHistArray 
+
+def getLabel(name):
+
+    return name.split('\\')[-1].replace('.jpg','')[:-2]
+
+def creatDataSet(path):
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+    imgLib.cutImageFromCircle(gray)  
+    gray_hist_array = gray2GrayHist(img)
+    lable = getLabel(path)
+    return gray_hist_array,lable
+
+
+def loadDataSet(fileList):
+    dataSets=[]
+    lables = []
+    for item in fileList:
+        gray_hist_array,lable = creatDataSet(item)
+        dataSets.append(gray_hist_array)
+        lables.append(lable)
+    return dataSets,lables
+
+
 if __name__ == '__main__':
+    
+    fileLists = [dir_path+'\\'+item for item in fileList]
+    testData,testlable= creatDataSet('.\database\changshengBlue1.jpg')
+    dataSets,lables=loadDataSet(fileLists)
+    result = knn.classify0(testData,dataSets,lables,3)
+
+    print result
+    print testlable
+    exit()
     trainDatas = []
     lables = []
     for item in fileList:
