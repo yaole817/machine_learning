3aa45d22c1b2d4df300e025adddea1943b9f0d73
yaole817 <yaole817@163.com>
2017-04-25 19:50:45 +0800

    basic implement the algorithm, the accuracy is low
    Signed-off-by: yaole817 <yaole817@163.com>

--- classifyBottle/videoRun.py
+++ classifyBottle/videoRun.py
@@ -3,37 +3,56 @@ import cv2
 import src.img as imgLib
 import src.imgFilter as imgFilter
 import os
+import src.kNN as knn
 
 
 dir_path = '.\\res'
 video_path = dir_path+'\\'+'test.mp4'
 
 img_dirPath = '.\\database'
-imgPathList =  [x for x in os.listdir(img_dirPath) if os.path.splitext(x)[1]=='.jpg']
+imgPathList =  [img_dirPath+'\\'+x for x in os.listdir(img_dirPath) if os.path.splitext(x)[1]=='.jpg']
 
 
 if __name__ == '__main__':
     dataSets = []
     dataLables = []
-
+    '''
+        this code is for load database
+        create the db
+    '''
     for item in imgPathList:
         print item
-        dataset,lable = imgFilter.loadDataSet(item)
-        dataSets.append(dataset)
+        img = cv2.imread(item)
+        #cv2.imshow('img',img)
+        #cv2.waitKey(0)
+        baseImg = imgFilter.BaseImg(img,item)
+        gray = baseImg.cutGrayFromCircle(img)
+        grayHistArray = baseImg.gray2GrayHist(gray)
+        lable = baseImg.getLable()
+        dataSets.append(grayHistArray)
         dataLables.append(lable)
 
-
+    '''
+        this is for test img
+    '''
     cap = cv2.VideoCapture(video_path)
     while(cap.isOpened()):
         ret, frame = cap.read()
         frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
         videoImg = imgFilter.BaseImg(frame)
         testSet  = videoImg.createDataSets()
-        for box in videoImg.colorImgBox:
+        print len(testSet)
+        print len(videoImg.colorImgBox)
+        for i in range(len(videoImg.colorImgBox)-1):
+            box = videoImg.colorImgBox[i]
+            test_img = testSet[i]
+
+            result_lable = knn.classify0(test_img,dataSets,dataLables,3)
+
             cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
             imgCenter  = (box[2] - box[0])/2+box[0]
-            cv2.putText(frame,"Hello World !", (imgCenter[0],imgCenter[1]),cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 0, 255),2)
-        
+            cv2.putText(frame,result_lable, (imgCenter[0],imgCenter[1]),cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 0, 255),2)
+            
         cv2.imshow('frame',videoImg.img)
         cv2.waitKey(0)
         if cv2.waitKey(1) & 0xFF == ord('q'):
